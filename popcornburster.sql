98212f790625439fb87b60584b896e4fce9a4690
Walter Ferrufino <fmwalter@hotmail.com>
2022-12-30 11:06:58 -0300

    se facilita la creacion de la base de datos para quien corrija

--- popcornburster.sql
+++ popcornburster.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE popcornburster;
+
+USE popcornburster;
+
+CREATE TABLE users (
+    id int AUTO_INCREMENT,
+    username varchar(30) NOT NULL,
+    password varchar(30) NOT NULL,
+    name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    signup_date date,    
+    PRIMARY KEY (id)
+);
+
+INSERT INTO users
+    (username, password, name, last_name, signup_date)
+    VALUES ("juan", "1234", "Juan", "Perez", now());
+
 SELECT * FROM popcornburster.users;
